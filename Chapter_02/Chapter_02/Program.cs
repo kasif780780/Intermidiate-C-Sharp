36942bbde0e7f4c1590e7c57fbb3c0953d42fef2
Kasif Khan <kasif780780@gmail.com>
2018-04-30 13:52:33 +0530

    first Commit

---
+++ Chapter_02/Chapter_02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1,"asif");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+            Console.ReadLine();
+        }
+    }
+}
