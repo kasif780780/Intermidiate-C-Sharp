1d871d9118c2bb49f6bb54217dea887ef727bffa
Kasif Khan <kasif780780@gmail.com>
2018-04-30 13:08:26 +0530

    First Commit

---
+++ Chapter01/Chapter01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter01
+{
+
+
+    public class Person
+    {
+        
+
+        public void Introduce(string v)
+        {
+            Console.WriteLine("Hi Am {0}",v);
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+           
+            person.Introduce("Asif");
+        }
+    }
+}
